1f2bdfd6802ea8d26b74e1c7cb0f71537ba7a4d0
Yakushyn Mark <yakushyn.mark@gmail.com>
2023-09-27 16:37:02 +0200

    second task completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const playerEl = document.getElementById('vimeo-player');
 
+const player = new Player(playerEl);
+const videoSavedTime = localStorage.getItem('videoplayer-current-time');
+
+if (videoSavedTime) {
+  player.setCurrentTime(videoSavedTime);
+}
+
+const throttledVideoProgress = throttle(({ seconds }) => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+  console.log(localStorage);
+}, 1000);
+
+player.on('timeupdate', throttledVideoProgress);
